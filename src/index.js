3b2880d9bfdb67dc6a6d609ecec150fa6f7b3e3d
Chao Wei Chiu <chaoweichiu@CW-2.local>
2016-11-08 08:21:06 -0500

    config components, added youtu-search-api

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+import React from 'react'
+import R from 'react-dom'
+
+const youtubeAPIKey = 'AIzaSyAqqNfDrWoYBJy9pduS6NQAUI5DMWgtGps'
+
 // Create new coponent 
 // This component shou;d produce html
-const App = function(){
+const App = ()=>{
 	return <h1>Hi</h1>
 }
 
 // Put into DOM
+
+R.render(<App />)
